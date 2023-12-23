668f0f6b6b89b26938567e9169595c36e3d777b3
Luisa Orozco <l.orozco@esciencecenter.nl>
2023-12-23 22:15:54 +0100

    Add solution 12 part 2

--- 2023/12/sol12.jl
+++ 2023/12/sol12.jl
@@ -61,7 +61,8 @@ sol_example_1 = sol_part_1("./input_example.txt")
 sol_puzzle_part_1 = sol_part_1("./input_puzzle.txt")
 println(" Part 1, example: $sol_example_1 \n Part 1, puzzle: $sol_puzzle_part_1")
 
-# Part 2: is of course to inneficient to solve even the example
+# Part 2
+
 function unfold(records)
     unfolded_records = []
     for record in records
@@ -73,18 +74,42 @@ function unfold(records)
     return unfolded_records
 end
 
+cache = Dict()
+function count_arrangements(row, groups)
+    result = 0
+    if isempty(groups)
+        return '#' in row ? 0 : 1
+    end
+    key = (row, groups)
+    if key in keys(cache)
+       return cache[key]
+    end
+    length(row) < sum(groups) + length(groups) - 1 && return 0
+    if !('.' in row[begin:groups[begin]])
+        if length(row) == groups[begin] || row[groups[begin]+1] != '#'
+            result += count_combinations(row[groups[begin] + 2:end], groups[begin+1:end])
+        end
+    end
+    if row[begin] != '#'
+        result += count_combinations(row[begin + 1:end], groups)
+    end
+    cache[key] = result
+    return result
+end
+
 function sol_part_2(input_file)
     records = unfold(readlines(input_file))
     sum_arrangements = 0 
     for record in records
-        arr_record = get_num_arrangements(record)
+        row, groups = split(record, " ")
+        groups = parse.(Int64, split(groups, ","))
+        arr_record = count_arrangements(row, groups)
         println(arr_record)
         sum_arrangements += arr_record
     end
     return sum_arrangements
 end
 
-#sol_example_2 = sol_part_2("./input_example.txt")
-#println(" Part 2, example: $sol_example_2")
-#sol_puzzle_part_2 = sol_part_2("./input_puzzle.txt")
-#println(" Part 2, example: $sol_example_2 \n Part 2, puzzle: $sol_puzzle_part_2")
+sol_example_2 = sol_part_2("./input_example.txt")
+sol_puzzle_part_2 = sol_part_2("./input_puzzle.txt")
+println(" Part 2, example: $sol_example_2 \n Part 2, puzzle: $sol_puzzle_part_2")
